d0e38e6c097074e2ac78d4fea77a5a20b3bfe10e
EngCode <elampratorabdallah14@gmail.com>
2023-05-03 03:16:36 +0300

    update.

--- template-parts/post/info.php
+++ template-parts/post/info.php
@@ -4,21 +4,24 @@
      * @since Phenix WP 1.0
      * @return void
     */
-?>
 
-<div class="flexbox pdb-15 mb-30 divider-b fs-14">
-    <span class="tx-icon far me-15 fa-clock">14 Dec 2022</span>
-    <span class="tx-icon far me-15 fa-user icon-primary">Abdullah Ramadan</span>
+    //===> Get Post Information <===//
+    $post_author_id = get_post_field( 'post_author', $post->ID);
+    $post_author_name = get_the_author_meta( 'display_name', $post_author_id);
+?>
+<div class="flexbox fs-14 mb-30 align-center-y divider-y pdy-10">
+    <span class="tx-icon far me-15 fa-clock"><?php echo get_the_date('F j, Y', $post->ID); ?></span>
+    <span class="tx-icon far me-15 fa-user icon-primary"><?php echo $post_author_name; ?></span>
     <!-- Share -->
     <div class="px-dropdown inline-block ms-auto" data-position="bottom, end">
         <!-- Toggle Button -->
-        <button class="px-toggle reset-button far fa-share-alt tx-icon pdy-5 color-primary">Share</button>
+        <button class="px-toggle reset-button far fa-share-alt tx-icon pdy-5 color-behance">Share</button>
         <!-- Dropdown Target -->
         <ul class="px-dropdown-list reset-list bg-white fs-14 w-min-200 bx-shadow-dp-1 border-1 border-solid border-alpha-10 radius-md">
-            <li class="color-facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" class="tx-icon fab fa-facebook-square">Share via Facebook</a></li>
-            <li class="color-twitter"><a href="https://twitter.com/intent/tweet?url=link&text=<?php the_title(); ?>" class="tx-icon fab fa-twitter-square">Share via Twitter</a></li>
-            <li class="color-linkedin"><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&title=<?php the_title(); ?>" class="tx-icon fab fa-linkedin">Share via Linked-In</a></li>
-            <li class="color-whatsapp"><a href="whatsapp://send?text=<?php the_title(); ?>" class="tx-icon fab fa-whatsapp-square">Share via WhatsApp</a></li>
+            <li class="color-facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" class="tx-icon fab fa-facebook-square"><?php echo __("Share via Facebook","phenix"); ?></a></li>
+            <li class="color-twitter"><a href="https://twitter.com/intent/tweet?url=link&text=<?php the_title(); ?>" class="tx-icon fab fa-twitter-square"><?php echo __("Share via Twitter","phenix"); ?></a></li>
+            <li class="color-linkedin"><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&title=<?php the_title(); ?>" class="tx-icon fab fa-linkedin"><?php echo __("Share via Linked-In","phenix"); ?></a></li>
+            <li class="color-whatsapp"><a href="whatsapp://send?text=<?php the_title(); ?>" class="tx-icon fab fa-whatsapp-square"><?php echo __("Share via WhatsApp","phenix"); ?></a></li>
         </ul>
         <!-- // Dropdown Target -->
     </div>

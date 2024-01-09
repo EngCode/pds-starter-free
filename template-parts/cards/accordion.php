e0b2aed167503314b56b7732fd7f703726ec2540
EngCode <elampratorabdallah14@gmail.com>
2024-01-09 17:15:44 +0200

    add new template parts.

---
+++ template-parts/cards/accordion.php
@@ -0,0 +1,26 @@
+<?php
+    /**
+     * Phenix Blocks
+     * @since Phenix WP 1.0
+     * @return void
+    */
+
+    //===> Get Post Information <===//
+    $post_title = get_the_title();
+?>
+<!-- Accordion Item -->
+<div class="col accordion-item" itemtype="https://schema.org/FAQPage">
+    <div class="bg-white radius-sm border-1 border-solid border-alpha-15">
+        <!-- Heading -->
+        <h4 class="accordion-btn display-flex fs-15 pdx-20 pdy-10 mb-0 align-between align-center-y transition-fast mouse-pointer" itemprop="name">
+            <span><?php echo $post_title; ?></span>
+            <i class="fas fa-plus lineheight-100"></i>
+        </h4>
+        <!-- Content -->
+        <div class="accordion-content pd-20 hidden divider-t border-alpha-15 fs-14" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <?php echo the_content(); ?>
+        </div>
+        <!-- // Content -->
+    </div>
+</div>
+<!-- // Accordion Item -->
---
+++ template-parts/cards/testimonial.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     * Phenix Blocks
+     * @since Phenix WP 1.0
+     * @return void
+    */
+
+    //===> Get Post Information <===//
+    $post_title = get_the_title();
+	$post_thumbnail   = get_the_post_thumbnail_url($post->ID, 'full');
+	//===> Thumbnail Placeholder <===//
+	if ($post_thumbnail === false) { $post_thumbnail = 'https://via.placeholder.com/200x200.webp'; }
+?>
+<!-- Testimonial Card -->
+<div class="testimonail-card mb-30 col-12 col-md-6 col-lg-4">
+    <div class="content-box bg-white position-rv radius-md pd-25 pd-md-30 far fa-quote-left h-min-100">
+        <?php if (get_field('sound-file')) : ?>
+            <button data-audio="<?php echo get_field('sound-file'); ?>" class="btn square radius-circle far fa-volume-up position-ab pos-top-20 pos-end-20 primary small"></button>
+        <?php endif; ?>
+        <h3 class="fs-15 mb-15"><img src="<?php echo $post_thumbnail;?>" alt="" class="radius-circle me-10" width="30" height="30"> <?php echo $post_title; ?></h3>
+        <p class="color-gray fs-12"><?php echo strip_tags(get_the_content()); ?></p>
+    </div>
+</div>
+<!-- // Testimonial Card -->

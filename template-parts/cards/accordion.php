58b2c49933e31c28d91cd939bcf851ae26e54ae4
EngCode <elampratorabdallah14@gmail.com>
2024-04-09 11:07:25 +0200

    update v1.0.0

--- template-parts/cards/accordion.php
+++ template-parts/cards/accordion.php
@@ -9,15 +9,14 @@
     $post_title = get_the_title();
 ?>
 <!-- Accordion Item -->
-<div class="col accordion-item" itemtype="https://schema.org/FAQPage">
-    <div class="bg-component-lvl-1 radius-sm border-1 border-solid border-alpha-15">
+<div class="col accordion-item" data-animation="fadeInUp" itemtype="https://schema.org/FAQPage">
+    <div class="border-1 border-solid border-alpha-10 radius-lg bg-grade-primary-offwhite bg-grade-180">
         <!-- Heading -->
-        <h4 class="accordion-btn display-flex fs-15 pdx-20 pdy-10 mb-0 align-between align-center-y transition-fast mouse-pointer" itemprop="name">
+        <h4 class="display-flex flow-reverse divider-b align-center-y align-between mb-0 icon-end accordion-btn mouse-pointer headline fs-17 tx-uppercase tx-icon fas icon-lg pdt-10 pdb-10 pds-20 pde-10 fa-square-plus" itemprop="name">
             <span><?php echo $post_title; ?></span>
-            <i class="fas fa-plus lineheight-100"></i>
         </h4>
         <!-- Content -->
-        <div class="accordion-content pd-20 hidden divider-t border-alpha-15 fs-14" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+        <div class="accordion-content pdt-20 hidden pds-25 pde-25 pdb-25 fs-15" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
             <?php echo the_content(); ?>
         </div>
         <!-- // Content -->
--- template-parts/cards/testimonial.php
+++ template-parts/cards/testimonial.php
@@ -14,26 +14,32 @@
 
     //===> Get Study Information <===//
     $job_title = get_post_meta($post->ID, 'job-title', true);
+    $audio_file = get_post_meta($post->ID, 'sound-file', true);
 
 	//===> Thumbnail Placeholder <===//
 	if ($post_thumbnail === false) { $post_thumbnail = 'https://via.placeholder.com/200x200.webp'; }
 ?>
-<!-- Testimonial Card -->
-<div class="testimonial-card mb-30 col-12 col-md-6 col-lg-4 pdy-15">
-    <div class="content-box bg-component-lvl-1 color-component-lvl-1 position-rv radius-md pd-25 pd-md-30 h-min-100 border-1 border-alpha-10 border-solid">
+<!-- Block Start -->
+<div class="testimonial-card col-12 col-md-6 col-lg-4 pdy-15">
+    <div class="content-box bg-component-lvl-1 color-component-lvl-1 position-rv radius-md h-min-100">
         <!-- Client Info -->
-        <div class="flexbox flow-nowrap align-center-y pdb-15 divider-b">
+        <div class="flexbox flow-nowrap align-center-y pdx-25 pdy-15 divider-b">
             <!-- Avatar -->
-            <img src="<?php echo $post_thumbnail;?>" alt="" class="radius-circle me-15" width="60" height="60">
+            <img src="<?php echo $post_thumbnail;?>" alt="" class="radius-circle me-15" style="width: 3.75rem; height: 3.75rem;">
             <!-- info -->
             <div class="info col">
                 <!-- Name -->
-                <h3 class="fs-16 fs-md-17 mb-10 lineheight-100"><?php echo $post_title; ?></h3>
+                <h3 class="fs-16 mb-10 lineheight-100"><?php echo $post_title; ?></h3>
                 <!-- Job Title -->
-                <?php if (isset($job_title )) { echo '<p class="fs-12 fs-md-14">'.$job_title .'</p>'; } ?>
+                <?php if (isset($job_title )) { echo '<p class="fs-14 lineheight-100 mb-0">'.$job_title .'</p>'; } ?>
             </div>
+            <!-- Audio Button -->
+            <?php if (isset($audio_file) && $audio_file !== '') : ?>
+            <button type="button" class="btn square primary radius-circle fas fa-play ms-15" data-audio="<?php echo $audio_file;?>"></button>
+            <?php endif;?>
         </div>
-        <p class="color-gray fs-14 pdt-15"><?php echo strip_tags(get_the_content()); ?></p>
+        <!-- Content -->
+        <p class="color-gray fs-14 pd-25"><?php echo strip_tags(get_the_content()); ?></p>
     </div>
 </div>
-<!-- // Testimonial Card -->
+<!-- // Block End -->
